c54e6176e847d18c77a2912b7a3ec2898101d95c
Ethan Gutierrez <ethanlizzard1123@gmail.com>
2022-07-20 22:45:14 -0400

    Initial Commit

---
+++ GT1000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GT1000
+//
+//  Created by Ethan Gutierrez on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
